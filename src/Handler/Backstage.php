cddec2561bbea4cb25f1c3f0c85638b05f1cb918
Asao <kamei@workspot.jp>
2014-10-25 08:48:38 +0900

    add Backstage and UrlMap handlers that are taken from StackPHP's middleware.

---
+++ src/Handler/Backstage.php
@@ -0,0 +1,34 @@
+<?php
+namespace WScore\Pile\Handler;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class StackBackstage
+ *
+ * class is from the back-stage class below with respect.
+ * https://github.com/atst/stack-backstage
+ * 
+ * to demonstrate the difference between the middleware and handler
+ */
+class StackBackstage implements HttpKernelInterface
+{
+    protected $path;
+
+    public function __construct(HttpKernelInterface $app, $path)
+    {
+        $this->app = $app;
+        $this->path = $path;
+    }
+
+    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+    {
+        $path = realpath($this->path);
+
+        if (false !== $path) {
+            return new Response(file_get_contents($path), 503);
+        }
+    }
+}
---
+++ src/Handler/UrlMap.php
@@ -0,0 +1,73 @@
+<?php
+namespace WScore\Pile\Handler;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * URL Map Middleware, which maps kernels to paths
+ *
+ * Maps kernels to path prefixes and is insertable into a stack.
+ *
+ * @author
+ * Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
+ *
+ * class is from the back-stage class below with respect.
+ * https://github.com/stackphp/url-map
+ * 
+ * to demonstrate the difference between the middleware and handler
+ * 
+ */
+class UrlMap implements HttpKernelInterface
+{
+    const ATTR_PREFIX = "stack.url_map.prefix";
+
+    /**
+     * @var HttpKernelInterface[]
+     */
+    protected $map = array();
+
+    public function __construct( array $map = array())
+    {
+        if ($map) {
+            $this->setMap($map);
+        }
+    }
+
+    /**
+     * Sets a map of prefixes to objects implementing HttpKernelInterface
+     *
+     * @param array $map
+     */
+    public function setMap(array $map)
+    {
+        # Collect an array of all key lengths
+        $lengths = array_map('strlen', array_keys($map));
+
+        # Sort paths by their length descending, so the most specific
+        # paths go first. `array_multisort` sorts the lengths descending and
+        # uses the order on the $map
+        array_multisort($lengths, SORT_DESC, $map);
+
+        $this->map = $map;
+    }
+
+    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+    {
+        $pathInfo = rawurldecode($request->getPathInfo());
+        foreach ($this->map as $path => $app) {
+            if (0 === strpos($pathInfo, $path)) {
+                $server = $request->server->all();
+                $server['SCRIPT_FILENAME'] = $server['SCRIPT_NAME'] = $server['PHP_SELF'] = $request->getBaseUrl().$path;
+
+                $attributes = $request->attributes->all();
+                $attributes[static::ATTR_PREFIX] = $request->getBaseUrl().$path;
+
+                $newRequest = $request->duplicate(null, null, $attributes, null, null, $server);
+
+                return $app->handle($newRequest, $type, $catch);
+            }
+        }
+        return null;
+    }
+}
