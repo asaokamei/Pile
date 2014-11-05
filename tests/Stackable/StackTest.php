79a479db74afc59f3adeb0bbca0735259762112f
Asao <kamei@workspot.jp>
2014-11-05 16:50:23 +0900

    more testing to come.

--- tests/Stackable/StackTest.php
+++ tests/Stackable/StackTest.php
@@ -1,7 +1,16 @@
 <?php
 namespace tests\Stackable;
 
+use WScore\Pile\App;
+
+require_once( __DIR__.'/../autoloader.php' );
+
+$app = App::start();
+echo get_class($app);
+
+/*
 class StackTest extends \PHPUnit_Framework_TestCase
 {
 
 }
+    */
