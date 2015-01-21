906f82663dec44e211ffa083af029c527463d953
Asao <kamei@workspot.jp>
2015-01-21 13:49:28 +0900

    move boot process to boot.php as a closure.

--- app/app.php
+++ app/app.php
@@ -1,43 +1,13 @@
 <?php
-use Tuum\Locator\Container;
-use Tuum\Locator\Locator;
 use Tuum\Web\App;
 use Tuum\Web\Http\Request;
 
 require_once( dirname( __DIR__ ) . '/vendor/autoload.php' );
 
-$loc = new Locator(__DIR__.'/config');
-$loc->addRoot( dirname(__DIR__).'/vendor/tuum/stack/scripts');
-$app = new App(new Container($loc));
-
-/*
- * set up directories
- */
-$app->set(App::CONFIG_DIR,   __DIR__.'/config');
-$app->set(App::TEMPLATE_DIR, __DIR__.'/views');
-$app->set(App::RESOURCE_DIR, __DIR__.'/views');
-$app->set(App::DEBUG, true);
-
-/*
- * set up services
- */
-$app->setRenderer( $app->get('renderer') );
-
-/*
- * set up handlers
- */
-$app->push($app->get('error-handler'));
-$app->push($app->get('session-handler'));
-$app->push($app->get('route-handler'));
-$app->push($app->get('url-mapper-handler'));
-$app->push($app->get('not-found-handler'));
-
-/*
- * set up releases
- */
-$app->push($app->get('view-release'));
-$app->push($app->get('session-release'));
-$app->push($app->get('error-release'));
+/** @var \Closure $boot */
+/** @var App $app */
+$boot = include( __DIR__.'/boot.php' );
+$app  = $boot([]);
 
 $request  = Request::startGlobal();
 $response = $app->__invoke( $request );
---
+++ app/boot.php
@@ -0,0 +1,58 @@
+<?php
+use Tuum\Locator\Container;
+use Tuum\Locator\Locator;
+use Tuum\Web\App;
+
+return function( array $config ) {
+
+    /*
+     * set up default configuration.
+     */
+    $default_config = [
+        'config' => __DIR__.'/config',
+        'views'  => __DIR__.'/views',
+        'var'    => dirname(__DIR__).'/var',
+        'debug'  => false,
+    ];
+    $config += $default_config;
+
+    /*
+     * build $app.
+     */
+    $loc = new Locator($config['config']);
+    $loc->addRoot( dirname(__DIR__).'/vendor/tuum/stack/scripts');
+    $app = new App(new Container($loc));
+
+    /*
+     * set up directories
+     */
+    $app->set(App::CONFIG_DIR,   $config['config']);
+    $app->set(App::TEMPLATE_DIR, $config['views']);
+    $app->set(App::RESOURCE_DIR, $config['views']);
+    $app->set(App::DEBUG,        $config['debug']);
+
+    /*
+     * set up services
+     */
+    $app->setRenderer( $app->get('renderer') );
+
+    /*
+     * set up handlers
+     */
+
+    $app->push($app->get('error-handler'));
+    $app->push($app->get('session-handler'));
+    $app->push($app->get('route-handler'));
+    $app->push($app->get('url-mapper-handler'));
+    $app->push($app->get('not-found-handler'));
+
+    /*
+     * set up releases
+     */
+
+    $app->push($app->get('view-release'));
+    $app->push($app->get('session-release'));
+    $app->push($app->get('error-release'));
+
+    return $app;
+};
