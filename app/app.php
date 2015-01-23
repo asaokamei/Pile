bb729fd570fd382929c86fdc22bf48e330c223cb
Asao <kamei@workspot.jp>
2015-01-24 05:37:14 +0900

    pass the $config to $boot!
    maybe try to emit trace info...

--- app/app.php
+++ app/app.php
@@ -6,12 +6,13 @@ require_once( dirname( __DIR__ ) . '/vendor/autoload.php' );
 
 /** @var \Closure $boot */
 /** @var App $app */
+date_default_timezone_set('Asia/Tokyo');
 $config = [
-    'debug' => true,
+    'debug'  => true,
     'routes' => __DIR__.'/routes.php',
 ];
 $boot = include( __DIR__.'/boot.php' );
-$app  = $boot([]);
+$app  = $boot($config);
 
 $request  = Request::startGlobal();
 $response = $app->__invoke( $request );
--- app/views/errors/error.php
+++ app/views/errors/error.php
@@ -11,5 +11,10 @@
 <p>Don't know what exactly have happened, but some error has happened and that is why you are seeing this screen...</p>
 <p>sorry.</p>
 
+<?php
+if( isset($trace) && is_array($trace) ) {
+    var_dump($trace);
+}
+?>
 </body>
 </html>
