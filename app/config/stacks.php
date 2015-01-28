cfa8c21dd9e2a1dcb54d0246380e7c0c9580bd24
Asao <kamei@workspot.jp>
2015-01-28 09:09:24 +0900

    simplify: removed AppReleaseInterface.

--- app/config/stacks.php
+++ app/config/stacks.php
@@ -5,12 +5,12 @@ return [
      */
     'error-stack',
     'session-stack',
-    
+    'view-release',
+
     /*
      * handlers and releases
      */
     'route-handler',
     'url-mapper-handler',
     'not-found-handler',
-    'view-release',
 ];
