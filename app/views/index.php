60fc55e76743b4052cf80b76f82ffed55c000f02
Asao <kamei@workspot.jp>
2015-01-28 17:12:15 +0900

    elaborate sample links.

--- app/views/index.php
+++ app/views/index.php
@@ -4,12 +4,22 @@
 <h1>TuumPHP Framework</h1>
 <p>TuumPHP is yet-another micro framework for PHP.</p>
 
-<h3>Example Links</h3>
+<h3>URL Mappers</h3>
 <ul>
     <li><a href="tuum.html" >html file</a></li>
     <li><a href="tuum.txt" >text file</a></li>
     <li><a href="tuum.md" >markdown file (not found)</a></li>
     <li><a href="errors.php" >php exception thrown</a></li>
+</ul>
+
+<h3>Routes (closure style)</h3>
+<ul>
     <li><a href="closure">from closure</a></li>
     <li><a href="closure-view">from closure-view</a></li>
+</ul>
+
+<h3>Routed Controller</h3>
+<ul>
+    <li><a href="/sample/" >sample welcome</a></li>
+    <li><a href="/sample/tuum" >sample hello world</a></li>
 </ul>
