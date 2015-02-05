3a00a1fff039ea7a0c747160680a6795759b5448
Asao <kamei@workspot.jp>
2015-02-05 16:49:50 +0900

    add comments.

--- app/boot.php
+++ app/boot.php
@@ -6,52 +6,64 @@ use Tuum\Web\Web;
 
 return function( array $config ) {
 
-    /*
-     * set up default configuration.
+    /* ---------------------
+     * default configuration
      */
     $default_config = [
+        
+        // default routes file to set up router.
         'routes' => __DIR__.'/routes.php',
+        
+        // default config directory. 
         'config' => __DIR__.'/config',
+        
+        // default view/template directory.
         'views'  => __DIR__.'/views',
+        
+        // default document/resource directory.
+        'docs'   => __DIR__.'/views',
+        
+        // default var (cache, logs, etc.) directory.
         'var'    => dirname(__DIR__).'/var',
+        
+        // default debug is off. 
         'debug'  => false,
     ];
     $config += $default_config;
 
-    /*
-     * build $app.
+    /* ----------
+     * build $app
      */
     $loc = new Locator($config['config']);
     $loc->addRoot( dirname(__DIR__).'/vendor/tuum/web/scripts');
     $loc->addRoot( dirname(__DIR__).'/vendor/tuum/view/scripts');
     $app = new Web(new Container($loc));
 
-    /*
+    /* ------------------
      * set up directories
      */
     $app->set(App::CONFIG_DIR,   $config['config']);
     $app->set(App::TEMPLATE_DIR, $config['views']);
-    $app->set(App::RESOURCE_DIR, $config['views']);
+    $app->set(App::DOCUMENT_DIR, $config['docs']);
     $app->set(App::VAR_DATA_DIR, $config['var']);
     $app->set(App::DEBUG,        $config['debug']);
 
-    /*
+    /* ---------------
      * set up services
      */
     $app->set(App::LOGGER, $app->get('logger'));
     $app->set(App::ROUTER, $app->get('router'));
     $app->set(App::RENDER_ENGINE, $app->get('renderer') );
 
-    /*
-     * set up handlers
+    /* -------------
+     * set up stacks
      */
     $stacks = $app->get('stacks');
     foreach($stacks as $stack) {
         $app->push($app->get($stack));
-        
     }
 
-    /*
+    /* ---------------
      * read the routes.
      * the route files are out of the config directory. 
      */
