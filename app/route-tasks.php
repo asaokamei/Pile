1acdcd0543544463fb3e8ccac7159f89f4bebc73
Asao <kamei@workspot.jp>
2015-02-05 16:45:22 +0900

    use the setRenderRoot method to simplify the script.

--- app/route-tasks.php
+++ app/route-tasks.php
@@ -1,12 +1,13 @@
 <?php
 use Tuum\View\Tuum\Renderer;
-use Tuum\Web\App;
+use Tuum\Web\Web;
+
+/** @var Web $app */
 
 $task_dir = dirname(__DIR__).'/src/Tasks';
+
 $app->setConfigRoot($task_dir.'/scripts');
-/** @var Renderer $engine */
-$engine = $app->get(App::RENDER_ENGINE);
-$engine->locator->addRoot( $task_dir.'/views');
+$app->setRenderRoot($task_dir.'/views');
 
 return $app->get('tasks/setup', ['root' => 'demoTasks*']);
     
